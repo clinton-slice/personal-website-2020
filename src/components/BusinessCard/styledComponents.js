3f28855e8f2565f93a1f9ac8fde0b04e157bae16
clinton-slice <clinton@slice.is>
2020-04-08 02:50:54 -0400

    added components i missed in the MobileUIv1 pr

---
+++ src/components/BusinessCard/styledComponents.js
@@ -0,0 +1,44 @@
+import styled, { keyframes } from "styled-components";
+
+const imageAnimate = keyframes`
+  50% {
+    transform: translate(0, 20px);
+ }
+`;
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & .image {
+    width: 150px;
+    height: 150px;
+    box-sizing: border-box;
+    border: 5px white solid;
+    border-radius: 50%;
+    overflow: hidden;
+    box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+    transform: translatey(0px);
+    animation: ${imageAnimate} 3s ease-in-out infinite;
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+`;
+
+export const Name = styled.p`
+  font-size: 48px;
+  text-align: center;
+`;
+
+export const JobTitle = styled.p`
+  text-align: center;
+  opacity: 0.7;
+  margin: -40px 0 48px 0;
+  small {
+    display: block;
+  }
+`;
---
+++ src/components/Card/styledComponents.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  padding: 2rem;
+  max-width: 512px;
+  margin: auto;
+  background: ${({ color, theme }) => color || theme.color};
+  color: ${({ text, theme }) => text || theme.fontColor};
+  white-space: pre-line;
+`;
+
+export const Title = styled.header`
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+`;
+
+export const Date = styled.p`
+  margin: unset;
+  text-align: right;
+  font-weight: bold;
+  font-size: 12px;
+  text-transform: uppercase;
+  opacity: 0.7;
+`;
+
+export const ImageWrapper = styled.a`
+  text-align: center;
+`;
+
+export default Section;
